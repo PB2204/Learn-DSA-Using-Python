d28603ec51af88eb1e6fda87a1d64578c23290e7
Pabitra Banerjee <rockstarpabitra2204@gmail.com>
2023-07-18 01:57:57 +0530

    2nd Update

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
     const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    const dropdownElement = document.querySelector(".dropdown");
+    const navElement = document.querySelector(".hide-on-small-screen ul");
 
     if (screenWidth <= 760) {
-        const imgSrc = "images/menu.png"; // Replace with the actual image source
+        const menuImgSrc = "images/menu.png"; // Replace with the actual menu image source
+        const crossImgSrc = "images/cross.png"; // Replace with the actual cross image source
 
         const imgElement = document.createElement("img");
-        imgElement.src = imgSrc;
-        imgElement.alt = "Image description";
+        imgElement.src = menuImgSrc;
+        imgElement.alt = "Menu";
         imgElement.style.height = "60px";
         imgElement.style.width = "60px";
 
-        const dropdownElement = document.querySelector(".dropdown");
         dropdownElement.appendChild(imgElement);
+
+        dropdownElement.addEventListener("click", function () {
+            navElement.classList.toggle("nav-hidden");
+            navElement.classList.toggle("nav-visible");
+            imgElement.src = navElement.classList.contains("nav-visible") ? crossImgSrc : menuImgSrc;
+            imgElement.alt = navElement.classList.contains("nav-visible") ? "Cross" : "Menu";
+        });
     }
 });
